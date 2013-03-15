76ae8ad65fe8097b6fe0dc4cf67a20be20a502a1
Fatih YILMAZ <fthylmz@live.com>
2013-03-15 12:20:49 +0200

    Ev -> Ofis

--- app/assets/javascripts/store/social.js
+++
@@ -1,9 +0,0 @@
-/* twitter-tweet */
-!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-/* facebook-like */
-var uvOptions = {};
-	  (function() {
-	    var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
-	    uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/uGXm7g0No84Vf5z6TraJ1w.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
-	  })();
